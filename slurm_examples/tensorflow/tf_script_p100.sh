4e1dd53eebbdeb59e42b8010d991433824c57657
Abdullah Ghani <abdullah.ghani@rutgers.edu>
2018-07-12 15:20:25 -0400

    added more benchmarks

--- slurm_examples/tensorflow/tf_script_p100.sh
+++ slurm_examples/tensorflow/tf_script_p100.sh
@@ -5,14 +5,14 @@
 #SBATCH --job-name=mnist_p100_gpu1            # Assign an short name to your job
 #SBATCH --nodes=1                    # Number of nodes you require
 #SBATCH --ntasks=1                   # Total # of tasks across all nodes
-#SBATCH --gres=gpu:2                 # Number of GPUs
-#SBATCH --constraint=pascal
-#SBATCH --nodelist=pascal005
+#SBATCH --gres=gpu:1  				# Number of GPUs
+#SBATCH --nodelist=gpu[005-006]                
 #SBATCH --mem=6000                  # Real memory (RAM) required (MB)
 #SBATCH --time=03:30:00              # Total run time limit (HH:MM:SS)
 #SBATCH --output=slurm.%N.%j.p100_gpu1.out     # STDOUT output file
 #SBATCH --error=slurm.%N.%j.p100_gpu1.err      # STDERR output file (optional)
 #SBATCH --export=ALL                 # Export you current env to the job env
+
 
 module purge
 
