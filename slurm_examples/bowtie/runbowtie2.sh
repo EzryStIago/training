e9f1430d2b9f89c2b32824fb47bca5145187a2d9
Abdullah Ghani <35004752+Abdullah-Ghani@users.noreply.github.com>
2018-07-17 14:30:43 -0400

    Add files via upload

---
+++ slurm_examples/bowtie/runbowtie2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --partition=main             # Partition (job queue)
+#SBATCH --job-name=bowtie2_example          # Assign an short name to your job
+#SBATCH --nodes=1                    # Number of nodes you require
+#SBATCH --ntasks=1                   # Total # of tasks across all nodes
+#SBATCH --cpus-per-task=1            # Cores per task (>1 if multithread tasks)
+#SBATCH --mem=100                   # Real memory (RAM) required (MB)
+#SBATCH --time=00:05:00              # Total run time limit (HH:MM:SS)
+#SBATCH --output=slurm.%N.%j.out     # STDOUT output file
+#SBATCH --error=slurm.%N.%j.err      # STDERR output file (optional)
+#SBATCH --export=ALL                 # Export you current env to the job env
+
+ module load bowtie2		 # load the module from the already available list of modules
+
+bowtie2-build chrI.fa chrI.fa    # To run bowtie2, we first need to build a database (also known as an index) for our reference genome.
+ # Now we can align some Illumina reads from yeast to chromosome I from the yeast reference genome.
+bowtie2 --no-unal -x chrI.fa -U yeast.fastq -S yeast.sam  
