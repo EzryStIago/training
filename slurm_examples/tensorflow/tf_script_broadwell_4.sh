49ba09bdd4e10495f8f397f23b62d03384b71d81
Abdullah Ghani <35004752+Abdullah-Ghani@users.noreply.github.com>
2018-07-17 15:10:14 -0400

    Update tf_script_broadwell_4.sh

--- slurm_examples/tensorflow/tf_script_broadwell_4.sh
+++ slurm_examples/tensorflow/tf_script_broadwell_4.sh
@@ -22,6 +22,7 @@ module load py-data-science-stack/5.1.0-kp807 #loads the community Python module
 source activate tensorflow-1.8.0 #change to 1.7.0 of you wish to run that version of tensorflow
 export PYTHONPATH=$PYTHONPATH:/home/aag193/python_mnist/tf/models_1.8 
 #Tells python where to look for, for required packages 
+#change path to where you saved packages when you cloned the github repo
 # change to models_1.7 if you wish to run that versionmnist_1.8.0.py
 
 
